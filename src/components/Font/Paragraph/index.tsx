25aa86653999fde29f85a943051345199575bd7c
Malan Gunaratne <malan-g@outlook.com>
2024-02-09 06:36:33 +1300

    fix argument passed to clsx which was copy+pasted from the Heading component

--- src/components/Font/Paragraph/index.tsx
+++ src/components/Font/Paragraph/index.tsx
@@ -4,7 +4,7 @@ import styles from "./index.module.scss";
 import { clsx } from "clsx";
 
 const Paragraph: React.FC<ParagraphProps> = ({ size, children }) => {
-  const className = clsx(styles.heading, {
+  const className = clsx(styles.paragraph, {
     [styles.lg]: size === "lg",
     [styles.md]: size === "md",
     [styles.sm]: size === "sm",

6bbb50196352e18c3f9af0932f12893b98d26bb2
Malan Gunaratne <malan-g@outlook.com>
2024-02-10 17:18:38 +1300

    created placeholder homepage data and setup homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+import { Heading, SubHeading, Paragraph } from "@/components/Font";
+import Spacer from "@/components/Layout/Spacer";
+import { socials, SocialType } from "@/lib";
+import ExternalLink from "@/components/Links/ExternalLink";
+import { homepage } from "@/lib/pages";
+
 export default function Home() {
-  return <>hello world</>;
+  return (
+    <main>
+      <Heading size="lg">{homepage.title}</Heading>
+      <SubHeading size="sm">{homepage.subtitle}</SubHeading>
+      <Spacer size="sm" />
+      <Paragraph size="md">{homepage.body}</Paragraph>
+      <Spacer size="lg" />
+      <div className="socials">
+        {socials.map((social: SocialType) => (
+          <span key={social.label}>
+            <ExternalLink
+              key={social.label}
+              link={social.link}
+              title={`Visit my ${social.title} page`}
+              label={social.label}
+            />
+            <Spacer size="sm" />
+          </span>
+        ))}
+      </div>
+    </main>
+  );
 }
---
+++ src/lib/pages/homepage.ts
@@ -0,0 +1,5 @@
+export const homepage = {
+  title: "Malan\nGunaratne",
+  subtitle: "An experienced junior web developer",
+  body: "Specialises in full-stack development.\nInterested in UX and Human Computer Interaction.\nCurrently sharpening their frontend skills.\nLives in Auckland, New Zealand.",
+};
